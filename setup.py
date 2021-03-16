1b54981ca8c362b477df0d658a4fd603bcab8985
Daniele Rondina <geaaru@sabayonlinux.org>
2021-03-17 00:18:42 +0100

    setup.py: Use underscore on package name

--- setup.py
+++ setup.py
@@ -5,14 +5,18 @@ from setuptools import setup, find_packages
 from web_keepaliver import __version__
 
 setup(
-        name='web-keepaliver',
+        name='web_keepaliver',
         version=__version__,
         description='Web Monitoring System to Kafka',
         author='Daniele Rondina',
         author_email='%s' % (
             'geaaru@sabayonlinux.org',
         ),
-        packages=find_packages(exclude=['etc', 'systemd']),
+        packages=find_packages(exclude=[
+            'etc', 'systemd',
+            'dbrm', 'grafana',
+            'ci', 'tests'
+        ]),
         install_requires=[
             'PyYAML',
             'kafka-python',
